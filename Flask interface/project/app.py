21aae41a74f3a3942efe88a95619168894827933
WithUs-all <jina0603@outlook.com>
2020-09-14 23:58:06 +0800

    updated by jis0324

--- Flask interface/project/app.py
+++ Flask interface/project/app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for
 import psycopg2
 from configparser import ConfigParser
 import os
-import json
+import traceback
 import time
 
 app = Flask(__name__)
@@ -21,6 +21,50 @@ def index():
 
   if request.method == "POST":
     return 'return data'
+
+@app.route('/add_bookmark', methods=['POST'])
+def add_bookmark():
+  try:
+    sql_update_query = """Update catalogues set bookmark = %s where uid = %s"""
+    cursor.execute(sql_update_query, (True, request.json['uid']))
+
+    return 'True'
+  except:
+    print(traceback.print_exc())
+    return 'False'
+
+@app.route('/remove_bookmark', methods=['POST'])
+def remove_bookmark():
+  try:
+    sql_update_query = """Update catalogues set bookmark = %s where uid = %s"""
+    cursor.execute(sql_update_query, (False, request.json['uid']))
+
+    return 'True'
+  except:
+    print(traceback.print_exc())
+    return 'False'
+
+@app.route('/add_exclude', methods=['POST'])
+def add_exclude():
+  try:
+    sql_update_query = """Update catalogues set excludeYN = %s where uid = %s"""
+    cursor.execute(sql_update_query, (True, request.json['uid']))
+
+    return 'True'
+  except:
+    print(traceback.print_exc())
+    return 'False'
+
+@app.route('/remove_exclude', methods=['POST'])
+def remove_exclude():
+  try:
+    sql_update_query = """Update catalogues set excludeYN = %s where uid = %s"""
+    cursor.execute(sql_update_query, (False, request.json['uid']))
+
+    return 'True'
+  except:
+    print(traceback.print_exc())
+    return 'False'
 
 if __name__ == "__main__":
   # database connect
--- Flask interface/project/static/js/main.js
+++ Flask interface/project/static/js/main.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
     // setting table as Datatable
     let table = $('#products_table').DataTable({
         "dom": '<"top">rt<"bottom"lp><"clear">'
-        // "dom": '<"top">rt<"bottom"<"#table_info_wrapper.d-flex" and il>p><"clear">'
     });
 
     // Global filter type select event
@@ -12,277 +11,176 @@ $(document).ready(function () {
         let selected_filter_type = $(this).html();
         $('.global-search-wrapper button.global-search-sel-btn').html(selected_filter_type);
         $('.global-search-wrapper button.global-search-sel-btn').attr('filter_type', $(this).data('value'));
+        $('.global-search-wrapper .global-filter-input').val('');
     });
 
-    // Golbal filter
-    $('.global-search-wrapper .global-filter-input').on('change',function() {
-        let value = $(this).val();
-        var filteredData = table
-            .columns( [0, 1] )
-            .flatten()
-            .filter( function ( value, index ) {
-                return  $(this).text().indexOf("whatever") !== -1;
+    // Global filter
+    $('.global-search-wrapper .global-filter-input').on('keyup', function() {
+        // Event listener to the two range filtering inputs to redraw on input
+        table.draw();
+    });
+
+    // Category Filter
+    $('.criteria-search-wrapper .category_filter_input').on('keyup', function() {
+        let category_filter_key = $('.criteria-search-wrapper .category_filter_input').val();
+        table.columns(3).search(category_filter_key).draw();
+    });
+
+    // Brand Filter
+    $('.criteria-search-wrapper .brand_filter_input').on('keyup', function() {
+        let category_filter_key = $('.criteria-search-wrapper .brand_filter_input').val();
+        table.columns(2).search(category_filter_key).draw();
+    });
+
+    // Website Filter
+    $('.criteria-search-wrapper .website_filter_input').on('keyup', function() {
+        let category_filter_key = $('.criteria-search-wrapper .website_filter_input').val();
+        table.columns(1).search(category_filter_key).draw();
+    });
+
+    $('.onoffswitch .onoffswitch-checkbox').on('click', function() {
+        if ($(this).is(':checked')) {
+            $(this).attr('checked', true)
+        } else {
+            $(this).removeAttr('checked')
+        }
+
+        table.draw();
+    });
+
+//    // hover bookmark icon
+//    $("#products_table").on("mouseenter", ".tr-row td .fa-bookmark-o", function () {
+//        $(this).removeClass("fa-bookmark-o");
+//        $(this).addClass("fa-bookmark");
+//        $(this).addClass("pink-color");
+//    });
+//
+//    // hover cancel bookmark icon
+//    $("#products_table").on("mouseleave", ".tr-row td .fa-bookmark", function () {
+//        $(this).addClass("fa-bookmark-o");
+//        $(this).removeClass("fa-bookmark");
+//        $(this).removeClass("pink-color");
+//    });
+
+//    // hover exclude icon
+//    $("#products_table").on("mouseenter", ".tr-row td .fa-times", function () {
+//        $(this).addClass("text-danger");
+//    });
+//
+//    // exclude cancel bookmark icon
+//    $("#products_table").on("mouseleave", ".tr-row td .fa-times", function () {
+//        $(this).removeClass("text-danger");
+//    });
+
+    // click bookmark
+    $("#products_table").on("click", ".tr-row td .bookmark-icon", function () {
+        let bookmark_icon = $(this);
+        let product_uid = bookmark_icon.parent().parent().data('id');
+
+        if (bookmark_icon.hasClass("fa-bookmark-o")) {
+            $.ajax({
+                url: '/add_bookmark',
+                type: 'POST',
+                data: JSON.stringify({'uid' : product_uid}),
+                contentType: 'application/json;charset=UTF-8',
+            }).done(function (response) {
+                bookmark_icon.removeClass("fa-bookmark-o");
+                bookmark_icon.addClass("fa-bookmark");
+                bookmark_icon.addClass("pink-color");
+                bookmark_icon.find('span').html('bookmarked');
+                table.draw();
+
             });
+        } else {
+            $.ajax({
+                url: '/remove_bookmark',
+                type: 'POST',
+                data: JSON.stringify({'uid' : product_uid}),
+                contentType: 'application/json;charset=UTF-8',
+            }).done(function (response) {
+                bookmark_icon.addClass("fa-bookmark-o");
+                bookmark_icon.removeClass("fa-bookmark");
+                bookmark_icon.removeClass("pink-color");
+                bookmark_icon.find('span').html('');
+                table.draw();
+            });
+        }
     });
 
+    // click exclude
+    $("#products_table").on("click", ".tr-row td .exclude-icon", function () {
+        let exclude_icon = $(this);
+        let product_uid = exclude_icon.parent().parent().data('id');
 
-	$('#crawl_result_table tbody').on('click', '.claim', function () {
-		document.getElementById("claim_people_form").reset();
-		$('#claimModal #claim_sumbit_btn').attr('disabled', 'true');
-
-		$('#l_name_modal').val($('#l_name_search').val());
-		$('#heldin_modal').val($(this).parent().parent().children('.heldin-td').text())
-		$('#last_address_modal').val($(this).parent().parent().children('.last-address-td').text())
-		$('#property_id_modal').val($(this).parent().parent().children('.property-id-td').text())
-		$('#reported_by_modal').val($(this).parent().parent().children('.reported-by-td').text())
-		$('#amount_modal').val($(this).parent().parent().children('.amount-td').text())
-		$('#claimModal').modal();
-	});
-
-	$('#claimModal #customCheck').change(function () {
-		if ($('#claimModal #claim_sumbit_btn').attr('disabled')) {
-			$('#claimModal #claim_sumbit_btn').removeAttr('disabled');
-		} else {
-			$('#claimModal #claim_sumbit_btn').attr('disabled', 'true');
-		}
-	});
-
-	$("#search_form").submit(function (event) {
-		event.preventDefault(); //prevent default action 
-
-		$('.loading-section').removeClass('hide');
-		$('.crawl-section').addClass('loading');
-
-		let post_url = $(this).attr("action"); //get form action url
-		let request_method = $(this).attr("method"); //get form GET/POST method
-		let form_data = $(this).serialize(); //Encode form elements for submission
-		let thread_status = '';
-
-		pullData = setInterval(
-			function () {
-				$.ajax({
-					url: post_url,
-					type: request_method,
-					data: form_data + "&thread_status=" + thread_status,
-				}).done(function (result) { //
-					response = JSON.parse(result);
-					response_data = response[0];
-					thread_status = response[1];
-					let tbody = '';
-					
-					if (thread_status == 'end') {
-						clearInterval(pullData);
-						$('.loading-section').addClass('hide');
-						$('.crawl-section').removeClass('loading');
-						$('.crawl-section .table-div').removeClass('hide');
-					} else if (thread_status == 'crawling') {
-						if (response_data.length > 0) {
-							for (let i = 0; i < response_data.length; i++) {
-								tbody += '<tr>'
-								tbody += '<td> <button class="btn btn-info claim-btn claim" data-lname="' + form_data['l_name_search'] + '">Claim</button> </td>';
-								tbody += '<td class="name-td">' + response_data[i][0] + '</td>';
-								tbody += '<td class="heldin-td">' + response_data[i][1] + '</td>';
-								tbody += '<td class="last-address-td">' + response_data[i][2] + '</td>';
-								tbody += '<td class="property-id-td">' + response_data[i][3] + '</td>';
-								tbody += '<td class="reported-by-td">' + response_data[i][4] + '</td>';
-								tbody += '<td class="amount-td">' + response_data[i][5] + '</td>';
-								tbody += '</tr>';
-							}
-							console.log(tbody)
-							$('.loading-section').addClass('hide');
-							$('.crawl-section').removeClass('loading');
-							$('.crawl-section .table-div').removeClass('hide');
-						}
-					}
-
-					$('.crawl-section .table-div #crawl_result_table tbody').html(tbody);
-					if ($.fn.dataTable.isDataTable('#crawl_result_table')) {
-						$('#crawl_result_table').DataTable();
-					} else {
-						$('#crawl_result_table').DataTable({
-							"columns": [
-								{ "orderable": false },
-								null,
-								null,
-								null,
-								null,
-								null,
-								null
-							]
-						});
-					}
-				})
-			}
-			, 5000);
-	});
-
-	$("#claim_people_form").submit(function (event) {
-		event.preventDefault(); //prevent default action 
-		$('#claimModal #claim_sumbit_btn').attr('disabled', 'true');
-
-		$('.loading-section').removeClass('hide');
-		$('.crawl-section').addClass('loading');
-
-		let post_url = $(this).attr("action"); //get form action url
-		let request_method = $(this).attr("method"); //get form GET/POST method
-		let form_data = $(this).serialize(); //Encode form elements for submission
-
-		$.ajax({
-			url: post_url,
-			type: request_method,
-			data: form_data
-		}).done(function (response) { //
-			$('#claimModal').modal('hide');
-			swal("SUCCESS!", "The people data saved successfully!", "success");
-
-			$('.loading-section').addClass('hide');
-			$('.crawl-section').removeClass('loading');
-		});
-
-
-
-	});
+        if (exclude_icon.hasClass("text-danger")) {
+            $.ajax({
+                url: '/remove_exclude',
+                type: 'POST',
+                data: JSON.stringify({'uid' : product_uid}),
+                contentType: 'application/json;charset=UTF-8',
+            }).done(function (response) { //
+                exclude_icon.removeClass("text-danger");
+                exclude_icon.find('span').html('');
+                table.draw();
+            });
+        } else {
+            $.ajax({
+                url: '/add_exclude',
+                type: 'POST',
+                data: JSON.stringify({'uid' : product_uid}),
+                contentType: 'application/json;charset=UTF-8',
+            }).done(function (response) { //
+                exclude_icon.addClass("text-danger");
+                exclude_icon.find('span').html('excluded');
+                table.draw();
+            });
+        }
+    });
 
 });
-// $(document).ready(function () {
 
-// 	let pullData;
-// 	$('#crawl_result_table').DataTable();
+$.fn.dataTable.ext.search.push(
+    function( settings, data, dataIndex ) {
+        let filter_key = $('.global-search-wrapper .global-filter-input').val();
+        let filter_type = $(".global-search-sel-btn").html()
 
-// 	$('#crawl_result_table tbody').on('click', '.claim', function () {
-// 		document.getElementById("claim_people_form").reset();
-// 		$('#claimModal #claim_sumbit_btn').attr('disabled', 'true');
+        let interface_bookmark_flag = false;
 
-// 		$('#l_name_modal').val($('#l_name_search').val());
-// 		$('#heldin_modal').val($(this).parent().parent().children('.heldin-td').text())
-// 		$('#last_address_modal').val($(this).parent().parent().children('.last-address-td').text())
-// 		$('#property_id_modal').val($(this).parent().parent().children('.property-id-td').text())
-// 		$('#reported_by_modal').val($(this).parent().parent().children('.reported-by-td').text())
-// 		$('#amount_modal').val($(this).parent().parent().children('.amount-td').text())
-// 		$('#claimModal').modal();
-// 	});
+        if ($('.onoffswitch .onoffswitch-checkbox').attr('checked')) {
+            interface_bookmark_flag = true;
+        }
 
-// 	$('#claimModal #customCheck').change(function () {
-// 		if ($('#claimModal #claim_sumbit_btn').attr('disabled')) {
-// 			$('#claimModal #claim_sumbit_btn').removeAttr('disabled');
-// 		} else {
-// 			$('#claimModal #claim_sumbit_btn').attr('disabled', 'true');
-// 		}
-// 	});
+        if (filter_type == 'All') {
+            for (let i=0; i < data.length; i++) {
+                if (data[i].includes(filter_key)) {
+                    if (interface_bookmark_flag) {
+                        console.log(data[7],'bookmarked')
+                        console.log(data[7].includes('bookmarked'))
+                        if (data[7].includes('bookmarked')) {
+                            return true
+                        }
+                    } else {
+                        return true
+                    }
+                }
+            }
+            return false
+        } else {
+            let table_head_list = ['Product', 'Product URL', 'Brand', 'Category', 'Last Price', 'Current Price', '% Discount', 'Bookmark', 'Exclude'];
+            let thead_index = table_head_list.indexOf(filter_type);
+            if (data[thead_index].includes(filter_key)) {
+                if (data[thead_index].includes(filter_key)) {
+                    if (interface_bookmark_flag) {
+                        if (data[7] == 'bookmarked') {
+                            return true
+                        }
+                    } else {
+                        return true
+                    }
+                }
+            }
+            return false;
+        }
 
-// 	$("#search_form").submit(function (event) {
-// 		event.preventDefault(); //prevent default action 
-
-// 		$('.loading-section').removeClass('hide');
-// 		$('.crawl-section').addClass('loading');
-
-// 		let post_url = $(this).attr("action"); //get form action url
-// 		let request_method = $(this).attr("method"); //get form GET/POST method
-// 		let form_data = $(this).serialize(); //Encode form elements for submission
-// 		let thread_status = '';
-// 		let tbody_data = []
-// 		$('.crawl-section .table-div #crawl_result_table tbody').html('');
-
-// 		pullData = setInterval(
-// 			function () {
-// 				$.ajax({
-// 					url: post_url,
-// 					type: request_method,
-// 					data: form_data + "&thread_status=" + thread_status,
-// 				}).done(function (result) { //
-// 					response = JSON.parse(result);
-// 					response_data = response[0];
-// 					thread_status = response[1];
-
-// 					if ( response_data.length > 0 ) {
-// 						for (let i = 0; i < response_data.length; i++ )  {
-//               tr_data = response_data[i].prepend('<button class="btn btn-info claim-btn claim" data-lname="' + form_data['l_name_search'] + '">Claim</button>')
-// 							tbody_data.push(tr_data);
-// 						}
-// 					}
-// 					console.log(response_data);
-// 					console.log(tbody_data);
-// 					let tbody = '';
-					
-// 					if (thread_status == 'end') {
-// 						clearInterval(pullData);
-// 						$('.loading-section').addClass('hide');
-// 						$('.crawl-section').removeClass('loading');
-// 						$('.crawl-section .table-div').removeClass('hide');
-// 					} else if (thread_status == 'crawling') {
-// 						if (tbody_data.length > 0) {
-							
-// 							$('.loading-section').addClass('hide');
-// 							$('.crawl-section').removeClass('loading');
-// 							$('.crawl-section .table-div').removeClass('hide');
-
-// 							$(document).ready(function() {
-							    
-// 							    if ($.fn.dataTable.isDataTable('#crawl_result_table')) {
-// 							    	var table = $('#crawl_result_table').DataTable();
-// 									  table.destroy();
-//         							$('#crawl_result_table').empty();
-//         							$('#crawl_result_table').DataTable( {
-// 								        data: tbody_data,
-// 								        columns: [
-// 								            { title: "" },
-// 								            { title: "Name" },
-// 								            { title: "Position" },
-// 								            { title: "Office" },
-// 								            { title: "Extn." },
-// 								            { title: "Start date" },
-// 								            { title: "Salary" }
-// 								        ]
-// 								    } );
-// 								} else {
-// 									$('#crawl_result_table').DataTable( {
-// 								        data: tbody_data,
-// 								        columns: [
-// 								            { title: "" },
-// 								            { title: "Name" },
-// 								            { title: "Position" },
-// 								            { title: "Office" },
-// 								            { title: "Extn." },
-// 								            { title: "Start date" },
-// 								            { title: "Salary" }
-// 								        ]
-// 								    } );
-// 								}
-// 							} );
-// 						}
-// 					}
-
-					
-// 				})
-// 			}
-// 			, 5000);
-// 	});
-
-// 	$("#claim_people_form").submit(function (event) {
-// 		event.preventDefault(); //prevent default action 
-// 		$('#claimModal #claim_sumbit_btn').attr('disabled', 'true');
-
-// 		$('.loading-section').removeClass('hide');
-// 		$('.crawl-section').addClass('loading');
-
-// 		let post_url = $(this).attr("action"); //get form action url
-// 		let request_method = $(this).attr("method"); //get form GET/POST method
-// 		let form_data = $(this).serialize(); //Encode form elements for submission
-
-// 		$.ajax({
-// 			url: post_url,
-// 			type: request_method,
-// 			data: form_data
-// 		}).done(function (response) { //
-// 			$('#claimModal').modal('hide');
-// 			swal("SUCCESS!", "The people data saved successfully!", "success");
-
-// 			$('.loading-section').addClass('hide');
-// 			$('.crawl-section').removeClass('loading');
-// 		});
-
-
-
-// 	});
-
-// });
+    }
+);
